ff95e56d7cd3690e127d40de75f794a12c237d52
geardestroyer85 <geardestroyer85@gmail.com>
2025-02-27 22:08:26 -0500

    Fun chat for the Frontend part

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,25 +1,57 @@
 import React from "react";
 import { useEffect, useState } from 'react';
 import { ApiResponse } from '../shared/interface';
+import { io } from 'socket.io-client';
 
+const socket = io();
 
 function App() {
   const [greetings, setGreetings] = useState('');
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [messages, setMessages] = useState<string[]>([]);
 
-  const getHelloFromServer = () => {
+  useEffect(() => {
+    socket.on('connect', () => {
+      setIsConnected(true);
+    });
+
+    socket.on('disconnect', () => {
+      setIsConnected(false);
+    });
+
+    socket.on('events', (e) => {
+      setMessages((messages) => [e, ...messages]);
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.off('disconnect');
+      socket.off('events');
+    };
+  }, []);
+
+  const getGreetings = () => {
     fetch('/api/hello')
       .then(response => response.json())
       .then((data: ApiResponse) => setGreetings(data.message))
       .catch(error => console.error('Error fetching data:', error));
   }
 
+  const sendPing = () => {
+    socket.emit()
+  }
+
 
   return (
     <div>
-      <p>Client: <span><button onClick={getHelloFromServer}>Hi, server</button></span></p>
+      <p>Client: <span><button onClick={getGreetings}>Hi, server</button></span></p>
       <p>Server: {greetings}</p>
-      <p>Client: Real time chat?</p>
-      <p>Server: </p>
+      {
+        greetings && <>
+          <p>Client: Real time chat?</p>
+          <p>Server: { isConnected ? 'Sure, no problem' : 'Sorry, no dude'}</p>        
+        </>
+      }
     </div>
   );
 }
